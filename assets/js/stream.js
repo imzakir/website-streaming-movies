0272f53aa98cdb0c5e780f5d2497e72303b1012d
gagas <gagassss.protonmail.com>
2020-01-03 10:43:19 +0700

    fix iframe probs

--- assets/js/stream.js
+++ assets/js/stream.js
@@ -18,13 +18,34 @@ if(!imdb) {
   window.location.href = 'index.html'
 }
 
+function addLoadingAnimation(selector) {
+  $('.'+selector).append(`
+  <div class="load-animation">
+  <div class="col s12 xl12 l12 m12 mt-5 mb-5 align-center center-align">
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue-only">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+`)
+}
+
+addLoadingAnimation('load-movie')
 $.ajax({
   url: 'https://cors-anywhere.herokuapp.com/https://api.gdriveplayer.us/v1/imdb/'+imdb,
   type : 'get',
   dataType : 'json',
   success: function(result) {
     embedVideoUrl = 'http://database.gdriveplayer.us/player.php?imdb='+imdb
-    $('.movie-media').attr('src', result.player_url)
+    $('.movie-media').attr('src', embedVideoUrl)
     $('.title').text('title : '+result.Title)
     $('.released').text('released : '+result.Released)
     $('.duration').text('duration : '+result.Runtime)
@@ -35,6 +56,9 @@ $.ajax({
     $('.directors').text('directors : '+result.Director)
     $('.writers').text('writer : '+result.Writer)
     $('.productions').text('production : '+result.Production)
+    $('.movie-media').load(function() {
+      $('.load-animation').html('')
+    })
   }
 })
 
